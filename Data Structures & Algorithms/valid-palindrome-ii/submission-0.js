eccce36756f3765355ee99f15c80eaadaf0c7389
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2026-06-23 13:57:59 -0700

    Add: valid-palindrome-ii - submission-0

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-0.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    validPalindrome(s) {
+
+        let l =0;
+        let r = s.length -1;
+
+
+        const isPalindrome = (l, r, s) => {
+            while (l < r){
+                if (s[l] !== s[r]){
+                    return false;
+                } else {
+                    l++;
+                    r--;
+                }
+            }
+            return true;
+        }
+
+
+        while (l < r) {
+            if (s[l] !== s[r]){
+                return isPalindrome (l+1, r, s) || isPalindrome (l, r-1, s);
+            }
+            l++;
+            r--;
+        }
+        
+
+        return true;
+        
+
+
+    }
+}
